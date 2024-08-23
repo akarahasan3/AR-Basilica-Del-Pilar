1886dc0e342a7816d86dd04ea6fe763f9ff47c44
Ajdin <ajdin.ka1@gmail.com>
2024-08-23 09:32:46 +0200

    AR Project

---
+++ Unity/KatedralaUnity/Assets/akarahasan3_script.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class akarahasan3_script : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+
+    public GameObject videoPlane;
+    public GameObject videoPOI;
+
+    public GameObject slikaPlane;
+    public GameObject slikaPOI;
+
+    public GameObject tekst;
+    public GameObject tekstPOI;
+    public GameObject tekstPlane;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.name == "OrganoPOI")
+            {
+                videoPOI.SetActive(false);
+                videoPlane.SetActive(true);
+            }
+
+            if (hit.collider.gameObject.name == "VideoPlane")
+            {
+                videoPlane.SetActive(false);
+                videoPOI.SetActive(true);
+            }
+
+            if (hit.collider.gameObject.name == "SlikaPlane")
+            {
+                slikaPlane.SetActive(false);
+                slikaPOI.SetActive(true);
+            }
+
+            if (hit.collider.gameObject.name == "SandTimer")
+            {
+                slikaPlane.SetActive(true);
+                slikaPOI.SetActive(false);
+            }
+
+            if (hit.collider.gameObject.name == "SpousePOI")
+            {
+                tekstPOI.SetActive(false);
+                tekst.SetActive(true);
+                tekstPlane.SetActive(true);
+
+            }
+
+            if (hit.collider.gameObject.name == "Tekst" || hit.collider.gameObject.name == "PlaneText")
+            {
+                tekst.SetActive(false);
+                tekstPlane.SetActive(false);
+                tekstPOI.SetActive(true);
+            }
+
+
+        }
+
+    }
+
+
+}
---
+++ Unity/KatedralaUnity/Assets/akarahasan3_script2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class akarahasan3_script2 : MonoBehaviour
+{
+    // Use this for initialization
+    public GameObject helpTekst;
+    public string Url = "https://c2.etf.unsa.ba/course/view.php?id=44";
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void funkcijaOnClick()
+    {
+        Application.Quit();
+    }
+
+    public void funkcijaInfo()
+    {
+        Application.OpenURL(Url);
+        
+    }
+
+    public void funkcijaHelp()
+    {
+        if (helpTekst.activeInHierarchy == true)
+        {
+            helpTekst.SetActive(false);
+        }
+        else
+        {
+            helpTekst.SetActive(true);
+        }
+    }
+
+}
